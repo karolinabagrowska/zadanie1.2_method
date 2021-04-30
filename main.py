d385382c4035966027a23f5108b16b497cea1e73
Karolina Bagrowska <karolina.bagrowska@gmail.com>
2021-04-30 20:41:40 +0200

    RESTAPI

---
+++ main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/method")
+def method_get():
+    return {"method": "GET"}
+
+@app.put("/method")
+def method_put():
+    return {"method": "PUT"}
+
+@app.delete("method")
+def method_delete():
+    return {"method": "DELETE"}
+
+@app.options("method")
+def method_options():
+    return {"method": "OPTIONS"}
+
+@app.post("method", status_code=201)
+def method_post():
+    return {"method": "POST"}
+    
+
